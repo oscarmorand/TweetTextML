eb26a0936771d80b96077ba4e416363f93f3b5a6
Oscartichaud <74990811+oscarmorand@users.noreply.github.com>
2022-05-26 12:13:05 +0900

    finish cleaning text

--- src/EDA_TweetText.py
+++ src/EDA_TweetText.py
@@ -73,8 +73,8 @@ else:
     print("different lengths, NOT OK")
 print('\n')
 
-stopwords_list = nltk.corpus.stopwords.words('english')
-print("Stopwords in english: ",stopwords_list)
+stopwords = nltk.corpus.stopwords.words('english')
+print("Stopwords in english: ",stopwords)
 
 #############################################################################
 #
@@ -83,30 +83,60 @@ print("Stopwords in english: ",stopwords_list)
 #####################
 
 
-def clean_text(before, after, allWord):
-    if allWord:
-        before = before+"[A-Za-z:/.0-9]*"
+def clean_text(before, after, del_all_word, sub_word):
+    if del_all_word:
+        before = before + "[A-Za-z:/.0-9]*"
+    if not sub_word:
+        before = " " + before + " "
     for i in range(len(data)):
         data[i][1] = re.sub(before, after, data[i][1])
 
+def tokenize_text():
+    for i in range(len(data)):
+        data[i][1] = data[i][1].split()
+
+def stemming_text():
+    stemmer = nltk.stem.porter.PorterStemmer()
+    for i in range(len(data)):
+        for j in range(len(data[i][1])):
+            data[i][1][j] = stemmer.stem(data[i][1][j])
+
+def removing_small_words(min_len):
+    for i in range(len(data)):
+        temp = []
+        for j in range(len(data[i][1])):
+            if len(data[i][1][j]) > min_len:
+                temp.append(data[i][1][j])
+        data[i][1] = temp
+
+print("Before cleaning Data[0]: ", data[0])
 
 # remove all characters that are not letters
-clean_text("[^\(A-Za-z \)]", "", False)
+clean_text("[^\(A-Za-z \)]", "", False, True)
 # remove all words that begin with "http" in order to remove all links
-clean_text("http", "", True)
+clean_text("http", "", True, True)
 # shorten all blank spaces that are longer than 1 space
-clean_text("  *", " ", False)
+clean_text("  *", " ", False, True)
+
+print("After basic cleaning Data[0]: ", data[0])
 
 # remove all english stopwords
-stopwords = ""
-for i in range(len(stopwords_list)):
-    stopwords += stopwords_list[i]
-    if i < len(stopwords_list)-1:
-        stopwords += "|"
-#clean_text(stopwords, "", False)  Doesn't work now
+for word in stopwords:
+    clean_text(word, " ", False, False)
 
+print("After removing stopwords Data[0]: ", data[0])
 
-print("Data[0]: ", data[0])
+tokenize_text()
+
+print("After tokenization of words Data[0]: ", data[0])
+
+stemming_text()
+
+print("After stemming the words Data[0]: ", data[0])
+
+removing_small_words(3)
+
+print("After removing small words Data[0]: ", data[0])
 
 '''
 pd_dataframe = pd.DataFrame(data_with_target, columns=header)   # Create a dataframe object that can be used with pandas library functions
