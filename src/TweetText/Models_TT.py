3b22f05687875249eca031c22b84299beac4cc2b
Oscartichaud <74990811+oscarmorand@users.noreply.github.com>
2022-06-03 16:37:59 +0900

    put all model code that can work with all dataset into a single file

--- src/TweetText/Models_TT.py
+++ src/TweetText/Models_TT.py
@@ -10,6 +10,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import cross_validate, train_test_split
 import random
 import warnings
+import FinalProjectML.src.Models as models
 warnings.filterwarnings("ignore")
 
 header = []
@@ -23,48 +24,14 @@ data_with_target = []
 #
 #####################
 
-def test_split(model):
-    X_train, X_test, y_train, y_test = train_test_split(cv, targets, test_size=.2, stratify=targets,
-                                                        random_state=rand_st)
-    model.fit(X_train, y_train)
-    prediction = model.predict(X_test)
-    return accuracy_score(prediction, y_test), roc_auc_score(prediction, y_test)
-
-def cross_validation(model):
-    scorers = {'Accuracy': 'accuracy', 'roc_auc': 'roc_auc'}
-    scores = cross_validate(estimator=model, X=cv, y=targets, scoring=scorers, cv=n_cv)
-    return scores['test_Accuracy'].mean(), scores['test_roc_auc'].mean()
-
-rand_st = 1
 is_model_used = {
     "rf": True
 }
-models = {
-    "rf": RandomForestClassifier(n_estimators=10, random_state=rand_st)
-}
-model_complete_name = {"rf": "Random Forest"}
 
 is_evaluation_used = {
     "test_split": True,
     "cross_validation" : True
 }
-evaluations = {
-    "test_split": test_split,
-    "cross_validation": cross_validation
-}
-n_cv = 5
-
-
-def test_model(model):
-    for evaluation_name in evaluations:
-        if is_evaluation_used[evaluation_name]:
-            start_time = time.time()
-            print("We use", evaluation_name,"for the evaluation")
-            scores_acc, scores_auc = evaluations[evaluation_name](model)
-
-            print(model_complete_name[model_name], "Acc:", scores_acc)
-            print(model_complete_name[model_name], "AUC:", scores_auc)
-            print("Runtime:", time.time() - start_time,"\n")
 
 
 if __name__ == '__main__':
@@ -90,8 +57,5 @@ if __name__ == '__main__':
     count_vectorizer = CountVectorizer(stop_words='english')
     cv = count_vectorizer.fit_transform([row[1] for row in data])
 
-    for model_name in models:
-        if is_model_used[model_name]:
-            print("Let's build a", model_complete_name[model_name], "model!\n")
-            test_model(models[model_name])
-            print("End of the evaluations for the",model_complete_name[model_name], "model\n")
+    models.build_models(cv, targets, is_evaluation_used, is_model_used)
+
